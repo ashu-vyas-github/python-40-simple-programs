eb668de8833d4254c7b3fdf2abf94a81cc0c6ad6
Ashutosh Vyas <ashutoshavyas@gmail.com>
2021-04-28 18:34:10 +0530

    Initial commit. Hello World.

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: ashutosh
+
+A simple hello world program
+
+"""
+def main():
+
+    print('Hello World')
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
