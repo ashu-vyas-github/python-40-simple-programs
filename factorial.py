b998ce7cd6587081fef6cb4c0cb56c44e9946008
Ashutosh Vyas <ashutoshavyas@gmail.com>
2021-04-28 21:46:42 +0530

    Add docstring for functions.

--- factorial.py
+++ factorial.py
@@ -8,9 +8,26 @@ This is a simple program to compute factorial of a given number
 """
 import numpy
 
+# This is a recursive function
+
 
 def factorial(num):
-  # This is a recursive function-
+    """
+    This function calculates factorial of a number recursively.
+
+    n! = n*(n-1)*(n-2)*...*2*1
+
+    Parameters
+    ----------
+    num : uint64
+        Input positive integer to calcuate factorial.
+
+    Returns
+    -------
+    uint64
+        Factorial of input positive integer.
+
+    """
 
     if num == 1:
         return num
@@ -19,6 +36,15 @@ def factorial(num):
 
 
 def main():
+    """
+    The main function to execute upon call.
+
+    Returns
+    -------
+    int
+        returns integer 0 for safe executions.
+
+    """
 
     print("Program to calculate factorial of an input positive number")
 
