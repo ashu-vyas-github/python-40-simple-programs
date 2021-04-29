fd847b6bef8ef588e2f0a2feee899045de0a7193
Ashutosh Vyas <ashutoshavyas@gmail.com>
2021-04-29 21:33:27 +0530

    Create ascii_to_character.py

---
+++ ascii_to_character.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: ashutosh
+
+Program to find the character from an input ASCII value.
+
+Example: "97" in ASCII is character "a"
+
+"""
+
+
+def main():
+    """
+    The main function to execute upon call.
+
+    Returns
+    -------
+    int
+        returns integer 0 for safe executions.
+
+    """
+
+    print("Program to find the character from an input ASCII value.")
+
+    ascii_in_numeric = int(input("Enter ASCII value to find character: "))
+
+    print(chr(ascii_in_numeric))
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
